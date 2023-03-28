7619cb9e4ef6ab35d3921c23a9f77cb740a5bfa7
Suman Pathak <samcool909@gmail.com>
2023-03-28 19:05:07 +0530

    7:20:24

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,6 +1,6 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
-
+from . import schemas
 
 
 ####SECRET_KEY
@@ -21,3 +21,22 @@ def create_access_token(data: dict):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     
     return encoded_jwt
+
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token,SECRET_KEY, algorithms=ALGORITHM)
+
+        id = str(payload.get("users_id"))
+
+        if id is None:
+            raise credentials_exception
+        
+        token_data = schemas.TokenData(id=id)
+
+    except JWTError: 
+        raise credentials_exception
+
+def get_current_user():
+    
+
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends,status,HTTPException,Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session 
 from ..database import  get_db
 from .. import oauth2, schemas, models, utils
@@ -7,9 +8,11 @@ from .. import oauth2, schemas, models, utils
 router = APIRouter(tags=["Authentication"])
 
 @router.post("/login")
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), 
+          db: Session = Depends(get_db)): #user_credentials: schemas.UserLogin
 
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+    user = db.query(models.User).filter(
+        models.User.email == user_credentials.username).first()
 
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
