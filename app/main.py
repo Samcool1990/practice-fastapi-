1880798128c24b0946e29a11a327ca2bd2052116
Suman Pathak <samcool909@gmail.com>
2023-03-28 18:21:18 +0530

    jwt auth created & tested

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ from . import models,schemas,utils
 from .database import engine, get_db
 from sqlalchemy.orm import Session  
 
-from .routers import post, user
+from .routers import post, user,auth
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -23,7 +23,7 @@ app = FastAPI()
 
 app.include_router(post.router)
 app.include_router(user.router)
-
+app.include_router(auth.router)
 ############DB CONNECTION####################
 while True:
     try: 
