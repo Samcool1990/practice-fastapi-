0be09907e16a8954cd57c074688b6775bbe6cd69
Suman Pathak <samcool909@gmail.com>
2023-03-30 14:49:55 +0530

    jwt authentocation

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI,Response,status, HTTPException,Depends,APIRouter
 from sqlalchemy.orm import Session  
 from ..database import  get_db
-from .. import models, schemas, utils
+from .. import models, schemas, utils, oauth2
 from typing import Optional,List
 
 
