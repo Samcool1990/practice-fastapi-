60721cc5a2051eeeb6989b8186a480301966fddd
Suman Pathak <samcool909@gmail.com>
2023-03-31 13:00:46 +0530

    Enviorment variable migration done

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     database_port: str 
     database_password: str
     database_name: str = ''
-    database_username: str = ''
+    database_username: str 
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
@@ -16,4 +16,4 @@ class Settings(BaseSettings):
     class Config:
         env_file = ".env"
 
-settings  = Settings()
+settings  = Settings() # type: ignore
